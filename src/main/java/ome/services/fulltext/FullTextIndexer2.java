0c9eabf9ead000d71c588eb6bb366110699f29f3
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2019-02-18 12:06:17 +0000

    rewrite new indexer to manage sessions and tx's manually

--- src/main/java/ome/services/fulltext/FullTextIndexer2.java
+++ src/main/java/ome/services/fulltext/FullTextIndexer2.java
@@ -19,13 +19,33 @@
 
 package ome.services.fulltext;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import ome.model.IObject;
+import ome.model.meta.EventLog;
+import ome.util.DetailsFieldBridge;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.SetMultimap;
 
 import org.hibernate.CacheMode;
+import org.hibernate.FlushMode;
+import org.hibernate.Hibernate;
 import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.engine.SessionFactoryImplementor;
+import org.hibernate.jdbc.Work;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
 import org.hibernate.search.bridge.FieldBridge;
@@ -36,66 +56,54 @@ import org.quartz.TriggerBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.SetMultimap;
-
-import ome.api.IConfig;
-import ome.api.IQuery;
-import ome.model.IObject;
-import ome.model.meta.EventLog;
-import ome.model.meta.Session;
-import ome.parameters.Parameters;
-import ome.services.sessions.SessionManager;
-import ome.services.util.Executor;
-import ome.system.Login;
-import ome.system.Principal;
-import ome.system.Roles;
-import ome.system.ServiceFactory;
-import ome.util.DetailsFieldBridge;
 
 public class FullTextIndexer2 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FullTextIndexer2.class);
 
-    private final Date DELAY_FOR_STARTUP = DateBuilder.futureDate(1, DateBuilder.IntervalUnit.MINUTE);
-    private final Date DELAY_FOR_FIELD_BRIDGE_CONTENTION = DateBuilder.futureDate(10, DateBuilder.IntervalUnit.SECOND);
-    private final Date DELAY_FOR_NOTHING_NEW_TO_INDEX = DateBuilder.futureDate(2, DateBuilder.IntervalUnit.SECOND);
+    private static enum Event {
+        STARTUP, FIELD_BRIDGE_CONTENTION, NOTHING_NEW_TO_INDEX;
+    }
 
-    private static final ImmutableMap<String, String> ALL_GROUPS_CONTEXT = ImmutableMap.of(Login.OMERO_GROUP, "-1");
+    private static Date getWhen(Event reason) {
+        switch (reason) {
+        case STARTUP:
+            return DateBuilder.futureDate(1, DateBuilder.IntervalUnit.MINUTE);
+        case FIELD_BRIDGE_CONTENTION:
+            return DateBuilder.futureDate(10, DateBuilder.IntervalUnit.SECOND);
+        case NOTHING_NEW_TO_INDEX:
+            return DateBuilder.futureDate(2, DateBuilder.IntervalUnit.SECOND);
+        default:
+            throw new IllegalArgumentException("cannot provide date for: " + reason);
+        }
+    }
+
     private static final int BATCH_SIZE = 256;
 
     private static final String JOB_GROUP = "full-text indexer";
 
+    private static final Set<Class<? extends IObject>> INCLUDE_TYPES =
+            ImmutableSet.of(ome.model.core.Image.class,ome.model.containers.Project.class,
+                    ome.model.containers.Dataset.class, ome.model.screen.Plate.class, ome.model.screen.Screen.class,
+                    ome.model.screen.PlateAcquisition.class, ome.model.screen.Well.class);
+
     private final Scheduler scheduler;
-    private final Executor executor;
-    private final Principal principal;
+    private final SessionFactoryImplementor sessionFactory;
     private final FieldBridge bridge;
     private final String countKey;
 
     private final SetMultimap<String, Long> toIndex = HashMultimap.create();
     private final SetMultimap<String, Long> toPurge = HashMultimap.create();
 
-    private long eventLogId;
+    private long eventLogId = -1;
 
     private int jobId = 0;
 
-    public FullTextIndexer2(Scheduler scheduler, Executor executor, FieldBridge bridge, SessionManager sessionManager, Roles roles,
-            String countKey) {
+    public FullTextIndexer2(Scheduler scheduler, SessionFactoryImplementor sessionFactory, FieldBridge bridge, String countKey) {
         this.scheduler = scheduler;
-        this.executor = executor;
+        this.sessionFactory = sessionFactory;
         this.bridge = bridge;
         this.countKey = countKey;
-
-        final Principal rootIndexer = new Principal(roles.getRootName(), roles.getSystemGroupName(), "FullText");
-        final Session session = sessionManager.createWithAgent(rootIndexer, getClass().getSimpleName(), null);
-        this.principal = new Principal(session.getUuid(), rootIndexer.getGroup(), rootIndexer.getEventType());
-    }
-
-    public void setSchedulerFactory(SchedulerFactoryBean schedulerFactory) {
     }
 
     private void register(String method) {
@@ -107,7 +115,8 @@ public class FullTextIndexer2 {
         }
     }
 
-    private void register(String method, Date when) {
+    private void register(String method, Event reason) {
+        final Date when = getWhen(reason);
         try {
             scheduler.scheduleJob(createJob(method), TriggerBuilder.newTrigger().startAt(when).build());
             LOGGER.debug("registered job for execution at {}: {}", when, method);
@@ -128,44 +137,59 @@ public class FullTextIndexer2 {
     }
 
     public void start() {
-        LOGGER.debug("starting indexer");
+        LOGGER.info("starting indexer");
         try {
-            register("prepare", DELAY_FOR_STARTUP);
+            register("prepare", Event.STARTUP);
         } catch (Throwable t) {
             LOGGER.error("failed to start indexer", t);
         }
     }
 
     public void prepare() {
-        LOGGER.debug("adding any new REINDEX entries");
-        executor.execute(principal, new Executor.SimpleWork<Object>(this, "prepare") {
-            @Override
-            @Transactional(readOnly = false)
-            public Object doWork(org.hibernate.Session session, ServiceFactory services) {
-                getSqlAction().refreshEventLogFromUpdatedAnnotations();
-                return null;
-            }
-        });
+        final Session session = sessionFactory.openSession();
         try {
-            register("gather");
-        } catch (Throwable t) {
-            LOGGER.error("failed to continue indexer", t);
-        }
-    }
-
-    public void gather() {
-        LOGGER.debug("reviewing event log for new entries");
-        executor.execute(principal, new Executor.SimpleWork<Object>(this, "gather") {
-            @Override
-            @Transactional(readOnly = true)
-            public Object doWork(org.hibernate.Session session, ServiceFactory services) {
-                final IConfig iConfig = services.getConfigService();
-                final IQuery iQuery = services.getQueryService();
-                final String currentLogIdString = iConfig.getConfigValue(countKey);
-                eventLogId = currentLogIdString == null ? -1 : Long.parseLong(currentLogIdString);
-                final String hql = "FROM EventLog WHERE id > :id ORDER BY id";
-                final Parameters params = new Parameters().addId(eventLogId).page(0, BATCH_SIZE);
-                final List<EventLog> logEntries = iQuery.findAllByQuery(hql, params);
+            LOGGER.debug("adding any new REINDEX entries");
+            session.setFlushMode(FlushMode.COMMIT);
+            Transaction transaction = session.beginTransaction();
+            session.doWork(new Work() {
+                @Override
+                public void execute(Connection connection) throws SQLException {
+                    final Statement stmt = connection.createStatement();
+                    try {
+                        stmt.execute("SELECT updated_entities_note_reindex()");
+                    } finally {
+                        stmt.close();
+                    }
+                }
+            });
+            transaction.commit();
+            LOGGER.debug("reviewing event log for new entries");
+            session.setFlushMode(FlushMode.MANUAL);
+            transaction = session.beginTransaction();
+            session.doWork(new Work() {
+                @Override
+                public void execute(Connection connection) throws SQLException {
+                    final PreparedStatement stmt = connection.prepareStatement("SELECT value FROM configuration WHERE name = ?");
+                    stmt.setString(1, countKey);
+                    try {
+                        final ResultSet results = stmt.executeQuery();
+                        if (results.next()) {
+                            eventLogId = Long.parseLong(results.getString(1));
+                        }
+                    } finally {
+                        stmt.close();
+                    }
+                }
+            });
+            final String hql = "FROM EventLog WHERE id > :id ORDER BY id";
+            final Query query = session.createQuery(hql);
+            query.setMaxResults(BATCH_SIZE);
+            query.setParameter("id", eventLogId);
+            @SuppressWarnings("unchecked")
+            final List<EventLog> logEntries = (List<EventLog>) query.list();
+            if (logEntries.isEmpty()) {
+                LOGGER.debug("no new event log entries", logEntries.size());
+            } else {
                 LOGGER.debug("reviewing {} event log entries", logEntries.size());
                 for (final EventLog logEntry : logEntries) {
                     switch (logEntry.getAction()) {
@@ -181,16 +205,18 @@ public class FullTextIndexer2 {
                     }
                     eventLogId = logEntry.getId();
                 }
-                return null;
             }
-        });
+            transaction.rollback();
+        } finally {
+            session.close();
+        }
         try {
             if (!toIndex.isEmpty()) {
                 register("index");
             } else if (!toPurge.isEmpty()) {
                 register("purge");
             } else {
-                register("prepare", DELAY_FOR_NOTHING_NEW_TO_INDEX);
+                register("prepare", Event.NOTHING_NEW_TO_INDEX);
             }
         } catch (Throwable t) {
             LOGGER.error("failed to continue indexer", t);
@@ -198,42 +224,48 @@ public class FullTextIndexer2 {
     }
 
     public void index() {
-        LOGGER.debug("indexing objects: count = {}", toIndex.size());
+        LOGGER.info("indexing objects: count = {}", toIndex.size());
         if (DetailsFieldBridge.tryLock()) {
             DetailsFieldBridge.setFieldBridge(bridge);
         } else {
             LOGGER.info("failed to lock field bridge so will wait awhile");
             try {
-                register("index", DELAY_FOR_FIELD_BRIDGE_CONTENTION);
+                register("index", Event.FIELD_BRIDGE_CONTENTION);
             } catch (Throwable t) {
                 LOGGER.error("failed to continue indexer", t);
             }
             return;
         }
+        final ParserSession parserSession = new ParserSession();
+        final Session session = sessionFactory.openSession();
         try {
             DetailsFieldBridge.setFieldBridge(bridge);
-            executor.execute(ALL_GROUPS_CONTEXT, principal, new Executor.SimpleWork<Object>(this, "index") {
-                @Override
-                @Transactional(readOnly = false)
-                public Object doWork(org.hibernate.Session session, ServiceFactory services) {
-                    final FullTextSession fullTextSession = Search.getFullTextSession(session);
-                    fullTextSession.setCacheMode(CacheMode.IGNORE);
-                    for (final Map.Entry<String, Collection<Long>> typeAndIds : toIndex.asMap().entrySet()) {
-                        final String entityType = typeAndIds.getKey();
-                        final Collection<Long> entityIds = typeAndIds.getValue();
-                        final String hql = "FROM " + entityType + " WHERE id IN (:ids)";
-                        final Query query = fullTextSession.createQuery(hql);
-                        query.setParameterList("ids", entityIds);
-                        for (final Object object : query.list()) {
-                            fullTextSession.index(object);
-                        }
+            final FullTextSession fullTextSession = Search.getFullTextSession(session);
+            fullTextSession.setCacheMode(CacheMode.IGNORE);
+            fullTextSession.setFlushMode(FlushMode.COMMIT);
+            final Transaction transaction = fullTextSession.beginTransaction();
+            for (final Map.Entry<String, Collection<Long>> typeAndIds : toIndex.asMap().entrySet()) {
+                final String entityType = typeAndIds.getKey();
+                final Collection<Long> entityIds = typeAndIds.getValue();
+                final String hql = "FROM " + entityType + " WHERE id IN (:ids)";
+                final Query query = fullTextSession.createQuery(hql);
+                query.setParameterList("ids", entityIds);
+                query.setReadOnly(true);
+                for (final Object entity : query.list()) {
+                    if (INCLUDE_TYPES.contains(Hibernate.getClass(entity))) {
+                        LOGGER.debug("indexing {}:{}", entityType, ((IObject) entity).getId());
+                        fullTextSession.index(entity);
+                    } else {
+                        LOGGER.debug("skipping {}:{}", entityType, ((IObject) entity).getId());
                     }
-                    toIndex.clear();
-                    return null;
                 }
-            });
+            }
+            transaction.commit();
+            toIndex.clear();
         } finally {
             DetailsFieldBridge.unlock();
+            session.close();
+            parserSession.closeParsedFiles();
         }
         try {
             if (!toPurge.isEmpty()) {
@@ -247,45 +279,45 @@ public class FullTextIndexer2 {
     }
 
     public void purge() {
-        LOGGER.debug("purging objects: count = {}", toPurge.size());
+        LOGGER.info("purging objects: count = {}", toPurge.size());
         if (DetailsFieldBridge.tryLock()) {
             DetailsFieldBridge.setFieldBridge(bridge);
         } else {
             LOGGER.info("failed to lock field bridge so will wait awhile");
             try {
-                register("purge", DELAY_FOR_FIELD_BRIDGE_CONTENTION);
+                register("purge", Event.FIELD_BRIDGE_CONTENTION);
             } catch (Throwable t) {
                 LOGGER.error("failed to continue indexer", t);
             }
             return;
         }
+        final Session session = sessionFactory.openSession();
         try {
-            executor.execute(ALL_GROUPS_CONTEXT, principal, new Executor.SimpleWork<Object>(this, "purge") {
-                @Override
-                @Transactional(readOnly = false)
-                public Object doWork(org.hibernate.Session session, ServiceFactory services) {
-                    final FullTextSession fullTextSession = Search.getFullTextSession(session);
-                    fullTextSession.setCacheMode(CacheMode.IGNORE);
-                    for (final Map.Entry<String, Collection<Long>> typeAndIds : toPurge.asMap().entrySet()) {
-                        final String entityType = typeAndIds.getKey();
-                        final Collection<Long> entityIds = typeAndIds.getValue();
-                        final Class<? extends IObject> entityClass;
-                        try {
-                            entityClass = Class.forName(entityType).asSubclass(IObject.class);
-                        } catch (ClassNotFoundException e) {
-                            LOGGER.error("unknown entity type in event log: {}", entityType, e);
-                            continue;
-                        }
-                        for (final Long entityId :entityIds) {
-                            fullTextSession.purge(entityClass, entityId);
-                        }
-                    }
-                    toPurge.clear();
-                    return null;
+            DetailsFieldBridge.setFieldBridge(bridge);
+            final FullTextSession fullTextSession = Search.getFullTextSession(session);
+            fullTextSession.setCacheMode(CacheMode.IGNORE);
+            fullTextSession.setFlushMode(FlushMode.COMMIT);
+            final Transaction transaction = fullTextSession.beginTransaction();
+            for (final Map.Entry<String, Collection<Long>> typeAndIds : toPurge.asMap().entrySet()) {
+                final String entityType = typeAndIds.getKey();
+                final Collection<Long> entityIds = typeAndIds.getValue();
+                final Class<? extends IObject> entityClass;
+                try {
+                    entityClass = Class.forName(entityType).asSubclass(IObject.class);
+                } catch (ClassNotFoundException e) {
+                    LOGGER.error("unknown entity type in event log: {}", entityType, e);
+                    continue;
                 }
-            });
+                for (final Long entityId :entityIds) {
+                    LOGGER.debug("purging {}:{}", entityType, entityId);
+                    fullTextSession.purge(entityClass, entityId);
+                }
+            }
+            transaction.commit();
+            toPurge.clear();
         } finally {
             DetailsFieldBridge.unlock();
+            session.close();
         }
         try {
             register("note");
@@ -296,15 +328,34 @@ public class FullTextIndexer2 {
 
     public void note() {
         LOGGER.debug("noting event log entries as processed");
-        executor.execute(principal, new Executor.SimpleWork<Object>(this, "note") {
-            @Override
-            @Transactional(readOnly = false)
-            public Object doWork(org.hibernate.Session session, ServiceFactory services) {
-                final IConfig iConfig = services.getConfigService();
-                iConfig.setConfigValue(countKey, Long.toString(eventLogId));
-                return null;
-            }
-        });
+        final Session session = sessionFactory.openSession();
+        try {
+            session.setFlushMode(FlushMode.COMMIT);
+            final Transaction transaction = session.beginTransaction();
+            session.doWork(new Work() {
+                @Override
+                public void execute(Connection connection) throws SQLException {
+                    PreparedStatement stmt = connection.prepareStatement("UPDATE configuration SET value = ? WHERE name = ?");
+                    stmt.setString(1, Long.toString(eventLogId));
+                    stmt.setString(2, countKey);
+                    try {
+                        stmt.execute();
+                        if (stmt.getUpdateCount() == 0) {
+                            stmt.close();
+                            stmt = connection.prepareStatement("INSERT INTO configuration (name, value) VALUES (?, ?)");
+                            stmt.setString(1, countKey);
+                            stmt.setString(2, Long.toString(eventLogId));
+                            stmt.execute();
+                        }
+                    } finally {
+                        stmt.close();
+                    }
+                }
+            });
+            transaction.commit();
+        } finally {
+            session.close();
+        }
         try {
             register("prepare");
         } catch (Throwable t) {
