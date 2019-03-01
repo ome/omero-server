319981a07f6ac0dda0851a81e9daa2a55325adad
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2019-03-01 14:25:04 +0000

    Deprecate graph util methods expected to be removed.

--- src/main/java/ome/services/graphs/ModelObjectSequencer.java
+++ src/main/java/ome/services/graphs/ModelObjectSequencer.java
@@ -36,7 +36,9 @@ import com.google.common.collect.Ordering;
  * This class groups the utility methods for performing such ordering.
  * @author m.t.b.carroll@dundee.ac.uk
  * @since 5.1.1
+ * @deprecated in anticipation of refactoring
  */
+@Deprecated
 public class ModelObjectSequencer {
     /**
      * Sort a list of original file IDs such that files precede containing directories.
