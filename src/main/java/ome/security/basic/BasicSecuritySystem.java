1716c38eee5995a11b7086a94fa18913b88f0bef
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2020-01-20 10:58:29 +0000

    Properly query for sudoer's Experimenter instance to copy.
    suggested by @joshmoore

--- src/main/java/ome/security/basic/BasicSecuritySystem.java
+++ src/main/java/ome/security/basic/BasicSecuritySystem.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.Set;
 
 import ome.system.EventContext;
-import ome.api.IQuery;
 import ome.api.local.LocalAdmin;
 import ome.api.local.LocalQuery;
 import ome.conditions.ApiUsageException;
@@ -34,7 +33,6 @@ import ome.model.meta.EventLog;
 import ome.model.meta.Experimenter;
 import ome.model.meta.ExperimenterGroup;
 import ome.model.meta.GroupExperimenterMap;
-import ome.parameters.Parameters;
 import ome.security.ACLVoter;
 import ome.security.AdminAction;
 import ome.security.EventProvider;
@@ -382,14 +380,7 @@ public class BasicSecuritySystem implements SecuritySystem,
             final ome.model.meta.Session session = ((SessionContext) ec).getSession();
             Experimenter sudoer = session.getSudoer();
             if (!(sudoer == null || sudoer.isLoaded())) {
-                final Long sudoerId = sudoer.getId();
-                final IQuery iQuery = sf.getQueryService();
-                final String hql = "SELECT omeName FROM Experimenter WHERE id = :id";
-                final Parameters params = new Parameters().addId(sudoerId);
-                final List<Object[]> result = iQuery.projection(hql, params);
-                final String sudoerName = (String) result.get(0)[0];
-                sudoer = new Experimenter(sudoerId, true);
-                sudoer.setOmeName(sudoerName);
+                sudoer = admin.userProxy(sudoer.getId());
                 session.setSudoer(sudoer);
             }
         }
