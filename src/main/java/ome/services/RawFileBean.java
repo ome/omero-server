23bb26ff6442277e0d248bab41fbd7fd3002d806
Chris Allan <callan@glencoesoftware.com>
2020-01-21 12:03:21 +0000

    Only flush if we are in read-write mode
    In certain conditions on certain filesystems flushing, even if there is
    nothing to flush and the file has been opened read-only, will result in
    an IOException.  Sometimes this will be very low level and other times
    just "permission denied".  There is no real reason to flush unless we
    are read-write so only do this when we are in that mode.
    
    We have noticed these types errors with repositories on NFSv4 with
    creative ACL usage as well as on CIFS.

--- src/main/java/ome/services/RawFileBean.java
+++ src/main/java/ome/services/RawFileBean.java
@@ -223,7 +223,9 @@ public class RawFileBean extends AbstractStatefulBean implements RawFileStore {
             final String path = buffer.getPath();
 
             try {
-                buffer.flush(true);
+                if (!buffer.getMode().equals("r")) {
+                    buffer.flush(true);
+                }
             } catch (IOException ie) {
                 final String msg = "cannot flush " + path + ": " + ie;
                 log.warn(msg);
